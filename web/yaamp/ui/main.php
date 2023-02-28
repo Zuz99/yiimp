268d4b76012c59fb99a87ce6e01b91c30ea0fe77
Autis <79344663+Zuz99@users.noreply.github.com>
2023-03-01 00:12:36 +0700

    Update main.php

--- web/yaamp/ui/main.php
+++ web/yaamp/ui/main.php
@@ -147,7 +147,7 @@ function showPageFooter()
     echo '<div class="footer">';
     $year = date("Y", time());
 
-    echo "<p>&copy; $year " . YAAMP_SITE_NAME . ' - ' . '<a href="https://github.com/Zuz99/yiimp_install_scrypt">Mod By Zuz99</a></p>';
+    echo "<p>&copy; $year " . YAAMP_SITE_NAME . ' - ' . '<a href="https://github.com/cryptopool-builders/Multi-Pool-Installer">Mod By Zuz99</a></p>';
 
     echo '</div><!-- footer -->';
 }
